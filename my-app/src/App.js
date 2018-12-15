18ad1a00a14b2cbaaf2a55d2fb5c9b3aa143ed08
Kayla Himmelberger <kaylahimmelberger@gmail.com>
2018-12-15 16:57:19 -0500

    added line back in that compares clicked ids to the full array.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,8 +10,16 @@ class App extends Component {
     cards: cards,
     selectedCards: [],
     score: 0,
-    highScore: 0
+    highScore: 0,
+    alert: "",
+    instructions: "The object of the game is to click on each card without clicking on the same card twice. See if you can collect all 16 cards!"
   };
+
+  countCard = id => {
+    // Filter this.state.cards for cards with an id not equal to the id being removed
+    const cards = this.state.cards.filter(card => card.id !== id);
+    console.log(cards)
+    }
 
   handleCardClick = id => {
     console.log(id)
@@ -22,23 +30,23 @@ class App extends Component {
     let newArray = this.state.cards;
     // Logic that checks the selectedCards to see if the current card selected exists in that array 
     if (clickedCards.includes(id) && newScore > newHighScore) {
-      // Logic that resets the game (the user as lost)
+      // Logic that resets the game (the user has lost)
       // Reassigns to initial empty array (the state of the array at the start of a new game)
       clickedCards = [];
       // alter the score dependent on a loss (reset to zero)
       newScore = 0;
-      // alter high score
-      newHighScore = newScore;
-      //add alert
-      newAlert = "You already chose that one--you lose."
-    
+      // show alert
+      newAlert = "You already chose that one--you lose.";
     } else {
       // Win logic (user clicked unclicked card)
       // Takes the current array and appends the clicked cards id
       clickedCards.push(id)
       // Alter the score dependent on a win (add to score)
       newScore++
-    }
+      // alter high score
+      newHighScore = newScore;
+    };
+
     newArray = this.shuffle(newArray)
     // Set this.state.cards equal to the new cards array
     this.setState({ 
@@ -47,7 +55,8 @@ class App extends Component {
       highScore: newHighScore,
       alert: newAlert, 
       cards: newArray });
-  };
+  }; 
+
 
   // Shuffling function
   shuffle = array => {
@@ -56,10 +65,7 @@ class App extends Component {
         [array[i], array[j]] = [array[j], array[i]];
     }
     return array;
-  }
-
-  // Check for high score
-
+  };
 
   // Map over this.state.cards and render a Card component for each card object
   render() {
@@ -67,8 +73,9 @@ class App extends Component {
       <Wrapper>
         <div>
           <Scoreboard>
-            <h3>Score: {this.state.score}</h3>
-            <h3>High Score: {this.state.highScore}</h3>
+            <h6>Instructions: {this.state.instructions}</h6>
+            <h3>Current Score: {this.state.score}</h3>
+            <h4>High Score: {this.state.highScore}</h4>
             {this.state.alert &&
             <h1>
               {this.state.alert}
@@ -90,7 +97,7 @@ class App extends Component {
     </div>
     </Wrapper>
     )
-  }
+  };
 };
 
 export default App;
